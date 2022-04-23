36f0ba605a95975307131d7875dc6e3feb420463
HunterLM <hunter109@gmail.com>
2022-04-23 09:46:34 -0500

    started project.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // Steps to Solve
     // 1. User answers questions
     // 2. Pass answers in the .then()
     // 3. Hand offf to Generate Markdown to create MD syntax
     // 4. pass MD to writeToFile
 
-// TODO: Include packages needed for this application
-
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [
+//     "Project Title?",
+//     "Description:",
+//     "Installation:",
+//     "Usage:",
+//     "Contribution Guidelines:",
+//     "Test Instructions:",
+// ];
+const questions = [
+    {
+        name: "project title",
+        message: "What is the Project Title?",
+        response: "string"
+    },
+    {
+        name: "description",
+        message: "Describle the Project:",
+        response: "string"
+    },
+    {
+        name: "installation",
+        message: "Installation Instructions:",
+        response: "string"
+    },
+    {
+        name: "usage",
+        message: "Usage Information:",
+        response: "string"
+    },
+    {
+        name: "contribute",
+        message: "Contribution Guidelines",
+        response: "string"
+    },
+    {
+        name: "test",
+        message: "Test Instructions",
+        response: "string"
+    },
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -20,6 +61,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     // write inquirer prompt here 
+    inquirer.prompt(questions);
     // call writeToFile inside my .then()
         // how to send answers from users into markdown functions?? 
     // pass answers to write to file here   (DATA)
