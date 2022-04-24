c1d11c0aee258ff449dda14045f3a3bca8fcf534
HunterLM <hunter109@gmail.com>
2022-04-23 19:01:06 -0500

    basically functional, need to accept input data, and validate data.

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // Steps to Solve
@@ -20,63 +21,67 @@ const questions = [
     },
     {
         type: "input",
-        name: "description",
         message: "Describle the Project:",
+        name: "description",
         // response: "string"
     },
     {
         type: "input",
-        name: "installation",
         message: "Installation Instructions:",
+        name: "installation",
         // response: "string"
     },
     {
         type: "input",
-        name: "usage",
         message: "Usage Information:",
+        name: "usage",
         // response: "string"
     },
     {
         type: "input",
-        name: "contribute",
         message: "Contribution Guidelines",
+        name: "contribute",
         // response: "string"
     },
     {
         type: "input",
-        name: "test",
         message: "Test Instructions",
+        name: "test",
         // response: "string"
     }
 ]
 
 // TODO: Create a function to write README file
-function writeToFile(projectTitle, description, installation, usage, contribute, test) { // do i need filename?
+function writeToFile(fileName, data) { // do i need filename?
     // use fs.writefile here
-    fs.writeFile(README.md, data)
-    // how do i use the markdown file here???
-    `
-    # ${projectTitle}
+    fs.writeFile(filename, data, err => {
+        if (err) {return console.log(err);}
+    })
 
-    ${description}
+    // fs.writeFile("README.md", answers)
+    // // how do i use the markdown file here???
+    // `
+    // # ${projectTitle}
 
-    ## Installation Instructions
+    // ${description}
 
-    ${installation}
+    // ## Installation Instructions
 
-    ## Usage
+    // ${installation}
 
-    ${usage}
+    // ## Usage
 
-    ## Contributing
+    // ${usage}
 
-    ${contribute}
+    // ## Contributing
 
-    ## Test
+    // ${contribute}
 
-    ${test}
+    // ## Test
 
-    `
+    // ${test}
+
+    // `
         // maybe a rest??? 
         // maybe import markdown functions here?????????? <=
 }
@@ -84,18 +89,18 @@ function writeToFile(projectTitle, description, installation, usage, contribute,
 // TODO: Create a function to initialize app
 function init() {
     // write inquirer prompt here 
-    inquirer.prompt(questions)
+    const answers = inquirer.prompt(questions)
     // call writeToFile inside my .then()
     // .then((data) => {
     //     writeToFile("README.md", data)
-    .then((answers) => fs.writeFileSync("README.md", writeToFile(fileName, answers)))
-    .then(() => console.log("Secessfully passed to writeToFile"))
-    .catch((err) => console.log(err))
+    .then((answers) => fs.writeFileSync("README.md", generateMarkdown(answers)))
+    // .then(() => writeFileAsync("README.md", markdown))
+    // .then(() => writeToFile())
+    .catch((err) => console.log(err));
     
 }
     // how to send answers from users into markdown functions?? 
     // pass answers to write to file here   (DATA)
-
 
 // Function call to initialize app
 init();
